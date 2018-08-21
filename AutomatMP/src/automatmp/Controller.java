130e7ef12b4cce2649500d93d62ada1dee9f96f5
ssprite28 <miguelrivera122@gmail.com>
2018-08-22 00:15:06 +0800

    Moved functions

--- AutomatMP/src/automatmp/Controller.java
+++ AutomatMP/src/automatmp/Controller.java
@@ -5,6 +5,8 @@
  */
 package automatmp;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author admin
@@ -14,6 +16,72 @@ public class Controller {
     private Model model;
     private View view;
     
+    public Controller (Model m, View v) {
+        this.model = m;
+        this.view = v;
+    }
     
+        public void board (ArrayList<String> origin, String entity) {
+        
+        if (model.getSpaceship().size() < 3){
+            origin.remove(entity);
+            model.getSpaceship().add(entity);
+            System.out.println("Added " + entity + " at index " + model.getSpaceship().size());
+        } 
+        else if (model.getSpaceship().size() == 3)
+            System.out.println("Spaceship is full!");
+       
+    }
+    
+    public void launch (ArrayList<String> origin, ArrayList<String> spaceship, ArrayList<String> destination) {
+        ArrayList<String> tempship = spaceship;
+        ArrayList<String> tempdest = destination;
+        
+        System.out.println("Origin: " + origin);
+        System.out.println("Spaceship: " + spaceship);
+        System.out.println("Destination: " + destination);
+        
+        if (spaceship.contains("Scientist")){
+            if (isValid(origin)) { //check if origin planet is valid
+                if (isValid(tempship)){ //check if spaceship is valid
+                    for (int j=0;j<tempship.size();j++) 
+                        tempdest.add(tempship.get(j));
+                    if (isValid(tempdest)){ //check if new destination is valid
+                        spaceship.clear();
+                        System.out.println("Transfer success");
+                    }
+                }
+                else
+                    System.out.println("Spacehip not allowed");
+            }
+            else
+                System.out.println("Origin planet not allowed");
+        } 
+        else if (!spaceship.contains("Scientist"))
+            System.out.println("Spaceship does not contain scientist!");
+    }
+    
+        public boolean isValid (ArrayList<String> planet) {
+        boolean allowed = true;
+        
+        if (!planet.contains("Scientist")) {
+            
+            if (planet.contains("Savage") && planet.contains("Cow")){
+                allowed = false;
+            } else if (planet.contains("Cow") && planet.contains("Grain")){
+                allowed = false;
+            } else if (planet.contains("Cow") && planet.contains("Lion")){
+                allowed = false;
+            } else if (planet.contains("Savage") && planet.contains("Lion")){
+                allowed = false;
+            } else if (planet.contains("Human") && planet.contains("Cow")){
+                allowed = false;
+            } else if (planet.contains("Human") && planet.contains("Lion")){
+                allowed = false;
+            }
+        }
+        
+        return allowed;
+    }
     
 }
--- AutomatMP/src/automatmp/Model.java
+++ AutomatMP/src/automatmp/Model.java
@@ -19,92 +19,19 @@ public class Model {
     private ArrayList<String> Earth;
     private ArrayList<String> Mars;
     
-    public Model (){
+    public Model () {
         this.Earth = new ArrayList<String>();
         this.Mars = new ArrayList<String>();
         this.spaceship = new ArrayList<String>();
         this.entities = new String[] {"Savage", "Cow", "Lion", "Grain", "Human", "Scientist"};
-           
+        
+        //initialize all entities to earth at the start
         Earth.add("Savage");
         Earth.add("Cow");
         Earth.add("Lion");
         Earth.add("Grain");
         Earth.add("Human");
         Earth.add("Scientist");
-    }
-    
-    
-    public void move (){
-        
-    }
-    
-    public boolean checkCompatible (String e1, String e2, String e3) {
-        boolean status = false;
-        
-        return status;
-    }
-    
-    public void board (ArrayList<String> origin, String entity){
-        
-        if (spaceship.size() < 3){
-            origin.remove(entity);
-            spaceship.add(entity);
-            System.out.println("Added " + entity + " at index " + spaceship.size());
-        } 
-        else if (spaceship.size() == 3)
-            System.out.println("Spaceship is full!");
-       
-    }
-    
-    public void launch (ArrayList<String> origin, ArrayList<String> spaceship, ArrayList<String> destination){
-        ArrayList<String> tempship = spaceship;
-        ArrayList<String> tempdest = destination;
-        
-        System.out.println("Origin: " + origin);
-        System.out.println("Spaceship: " + spaceship);
-        System.out.println("Destination: " + destination);
-        
-        if (spaceship.contains("Scientist")){
-            if (isValid(origin)) { //check if origin planet is valid
-                if (isValid(tempship)){ //check if spaceship is valid
-                    for (int j=0;j<tempship.size();j++) 
-                        tempdest.add(tempship.get(j));
-                    if (isValid(tempdest)){ //check if new destination is valid
-                        spaceship.clear();
-                        System.out.println("Transfer success");
-                    }
-                }
-                else
-                    System.out.println("Spacehip not allowed");
-            }
-            else
-                System.out.println("Origin planet not allowed");
-        } 
-        else if (!spaceship.contains("Scientist"))
-            System.out.println("Spaceship does not contain scientist!");
-    }
-    
-    public boolean isValid (ArrayList<String> planet) {
-        boolean allowed = true;
-        
-        if (!planet.contains("Scientist")){
-            
-            if (planet.contains("Savage") && planet.contains("Cow")){
-                allowed = false;
-            } else if (planet.contains("Cow") && planet.contains("Grain")){
-                allowed = false;
-            } else if (planet.contains("Cow") && planet.contains("Lion")){
-                allowed = false;
-            } else if (planet.contains("Savage") && planet.contains("Lion")){
-                allowed = false;
-            } else if (planet.contains("Human") && planet.contains("Cow")){
-                allowed = false;
-            } else if (planet.contains("Human") && planet.contains("Lion")){
-                allowed = false;
-            }
-        }
-        
-        return allowed;
     }
     
     public String[] getEntities () {
@@ -114,6 +41,16 @@ public class Model {
     public ArrayList<String> getSpaceship () {
         return spaceship;
     }
+    
+       public ArrayList<String> getEarth() {
+        return Earth;
+    }
+
+    public ArrayList<String> getMars() {
+        return Mars;
+    }
+    
+    
 
     
     
