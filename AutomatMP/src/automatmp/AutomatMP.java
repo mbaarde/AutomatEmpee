8dcfb0781c48f0cb35ea7071fe431ca96a461178
PaoDLR <36217674+PaoDLR@users.noreply.github.com>
2018-08-22 01:17:07 +0800

    view

--- AutomatMP/src/automatmp/AutomatMP.java
+++ AutomatMP/src/automatmp/AutomatMP.java
@@ -16,6 +16,12 @@ public class AutomatMP {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new View().setVisible(true);
+            }
+        });
     }
     
 }
